cd6bb15e3ddbf32aac4271451ece7989797c6e8f
Ankit Malik <ankitmalik844903@gmail.com>
2023-07-24 10:51:14 +0530

    add animation

--- src/components/core/AboutPage/LearningGrid.jsx
+++ src/components/core/AboutPage/LearningGrid.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+// importing aos
+import AOS from "aos";
+import "aos/dist/aos.css";
 import HighlightText from "../../../components/core/HomePage/HighlightText";
 import CTAButton from "../../../components/core/HomePage/Button";
 
@@ -46,10 +49,13 @@ const LearningGridArray = [
 
 const LearningGrid = () => {
   return (
-    <div className="grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12">
+    <div className='grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12'>
       {LearningGridArray.map((card, i) => {
         return (
           <div
+            data-aos='flip-up'
+            data-aos-duration='400'
+            data-aos-offset='100'
             key={i}
             className={`${i === 0 && "xl:col-span-2 xl:h-[294px]"}  ${
               card.order % 2 === 1
@@ -60,26 +66,36 @@ const LearningGrid = () => {
             } ${card.order === 3 && "xl:col-start-2"}  `}
           >
             {card.order < 0 ? (
-              <div className="xl:w-[90%] flex flex-col gap-3 pb-10 xl:pb-0">
-                <div className="text-4xl font-semibold ">
+              <div
+                data-aos='flip-left'
+                data-aos-duration='400'
+                data-aos-offset='100'
+                className='xl:w-[90%] flex flex-col gap-3 pb-10 xl:pb-0'
+              >
+                <div className='text-4xl font-semibold '>
                   {card.heading}
                   <HighlightText text={card.highlightText} />
                 </div>
-                <p className="text-richblack-300 font-medium">
+                <p className='text-richblack-300 font-medium'>
                   {card.description}
                 </p>
 
-                <div className="w-fit mt-2">
+                <div className='w-fit mt-2'>
                   <CTAButton active={true} linkto={card.BtnLink}>
                     {card.BtnText}
                   </CTAButton>
                 </div>
               </div>
             ) : (
-              <div className="p-8 flex flex-col gap-8">
-                <h1 className="text-richblack-5 text-lg">{card.heading}</h1>
+              <div
+                className='p-8 flex flex-col gap-8'
+                data-aos='flip-right'
+                data-aos-duration='400'
+                data-aos-offset='100'
+              >
+                <h1 className='text-richblack-5 text-lg'>{card.heading}</h1>
 
-                <p className="text-richblack-300 font-medium">
+                <p className='text-richblack-300 font-medium'>
                   {card.description}
                 </p>
               </div>
