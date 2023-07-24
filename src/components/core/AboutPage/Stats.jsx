4ca0eb6fc8579b96c40defd0b56991b5a112855d
Ankit Malik <ankitmalik844903@gmail.com>
2023-07-24 10:51:46 +0530

    add animation

--- src/components/core/AboutPage/Stats.jsx
+++ src/components/core/AboutPage/Stats.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Stats = [
-  { count: "5K", label: "Active Students" },
-  { count: "10+", label: "Mentors" },
-  { count: "200+", label: "Courses" },
-  { count: "50+", label: "Awards" },
+  { count: "5K", label: "Active Students", order: 1 },
+  { count: "10+", label: "Mentors", order: 2 },
+  { count: "200+", label: "Courses", order: 3 },
+  { count: "50+", label: "Awards", order: 4 },
 ];
 
 const StatsComponenet = () => {
+  useEffect(() => {
+    AOS.init(); // Initialize AOS library
+  }, []);
+
   return (
-    <div className="bg-richblack-700">
+    <div
+      className='bg-richblack-700'
+      data-aos='slide-up'
+      data-aos-duration='400'
+      data-aos-offset='100'
+    >
       {/* Stats */}
-      <div className="flex flex-col gap-10 justify-between w-11/12 max-w-maxContent text-white mx-auto ">
-        <div className="grid grid-cols-2 md:grid-cols-4 text-center">
-          {Stats.map((data, index) => {
-            return (
-              <div className="flex flex-col py-10" key={index}>
-                <h1 className="text-[30px] font-bold text-richblack-5">
-                  {data.count}
-                </h1>
-                <h2 className="font-semibold text-[16px] text-richblack-500">
-                  {data.label}
-                </h2>
-              </div>
-            );
-          })}
+      <div className='flex flex-col gap-10 justify-between w-11/12 max-w-maxContent text-white mx-auto '>
+        <div className='grid grid-cols-2 md:grid-cols-4 text-center'>
+          {Stats.map((data, index) => (
+            <div
+              className='flex flex-col py-10'
+              key={index}
+              data-aos={data.order < 3 ? "slide-right" : "slide-left"}
+              data-aos-duration='400'
+            >
+              <h1 className={`text-[32px] font-bold text-richblack-5`}>
+                {data.count}
+              </h1>
+              <h2 className={`font-semibold text-[18px] text-richblack-500`}>
+                {data.label}
+              </h2>
+            </div>
+          ))}
         </div>
       </div>
     </div>
