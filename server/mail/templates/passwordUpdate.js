6da3a137f1d4078e89ede931061f8279be9191e9
Ankit Malik <ankitmalik844903@gmail.com>
2023-07-27 23:45:39 +0530

    Update passwordUpdate.js

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -55,8 +55,8 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://study-alpha-plum.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+            <a href="https://code-infinity.vercel.app"><img class="logo"
+                    src="https://i.ibb.co/RDSMk5X/mail-logo.png" alt="CodeInfinity Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
