acb3f2f02ee74890bbde9aaf7c3a37ea694ebfa5
Ankit Malik <ankitmalik844903@gmail.com>
2023-07-28 00:01:08 +0530

    Update ResetPassword.js

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `https://study-alpha-plum.vercel.app/update-password/${token}`;
+		const url = `https://code-infinity.vercel.app/update-password/${token}`;
 
 		await mailSender(
 			email,
